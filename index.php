f2a90fac16e232b99be982bf77547b6b83761bdf
Dylan Luchmun <dylan.luchmun@epitech.eu>
2019-10-06 01:34:29 +0200

    code review

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
         <?php 
-
             session_start();
-            if(!isset($_SESSION['username'])) {
+            if (!isset($_SESSION['username'])) {
                 header('Location: login.php');
             }
 
-            require_once('header.php');
-            require_once('footer.php');
+            require_once('header.php'); 
+            require_once('footer.php'); 
             require_once('db/dbconf.php');
-            require_once('showProfilePic');
+            require_once('showProfilePic.php');
 
             include 'action.php';
-
         ?>
-
         <div id="nav-section">
             <nav class="navbar navbar-default">
                 <div class="container-fluid">
@@ -24,27 +21,17 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a href="#" class="navbar-brand">
-                            <h1 class="title-note">CRUD</h1>
-                        </a>
+                        <a class="navbar-brand" href="#"><h1 class="title-note">CRUD</h1></a>
                     </div>
+
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                    <img src="<?php echo $profilePic ?>" alt="profile" class="profile">
-                                    <span class="caret"></span>
-                                </a>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img class="profile" src="<?php echo $profilePic ?>" alt="profile"> <span class="caret"></span></a>
                                 <ul class="dropdown-menu">
-                                    <li>
-                                        <a>
-                                            <h4><?php echo $fullName ?></h4>
-                                        </a>
-                                    </li>
+                                    <li><a><h4><?php echo $fullName ?></h4></a></li>
                                     <li role="separator" class="divider"></li>
-                                    <li>
-                                        <a href="logout.php">Déconnexion</a>
-                                    </li>
+                                    <li><a href="logout.php">Déconnexion</a></li>
                                 </ul>
                             </li>
                         </ul>
@@ -52,13 +39,14 @@
                 </div>
             </nav>
         </div>
+
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
                     <a href="#addnew" class="btn btn-primary" data-toggle="modal"><span class="fa fa-plus"></span> Nouveau</a>
                     <?php 
                         session_start();
-                        if(isset($_SESSION['message'])) {
+                        if(isset($_SESSION['message'])){
                     ?>
                     <div class="alert alert-dismissible alert-success" style="margin-top:20px;">
                         <button type="button" class="close" data-dismiss="alert">&times;</button>
@@ -77,12 +65,12 @@
                             <th>Contrôle</th>
                         </thead>
                         <tbody>
-                            <?php 
+                            <?php
                             include_once('connection.php');
 
                             $database = new Connection();
                             $db = $database->open();
-                            try {
+                            try{	
                                 $sql = 'SELECT * FROM members';
                                 foreach ($db->query($sql) as $row) {
                             ?>
@@ -97,13 +85,12 @@
                                 </td>
                                 <?php include('edit_delete_modal.php'); ?>
                             </tr>
-                            <?php
+                            <?php 
                                 }
                             }
-                            catch(PDOException $e) {
-                                echo "Désolé, un problème est survenu lors de la connection " . $e->getMessage();
+                            catch(PDOException $e){
+                                echo "Désolé, il y a eu un problème lors de la connection: " . $e->getMessage();
                             }
-
                             $database->close();
                             ?>
                         </tbody>
@@ -111,8 +98,8 @@
                 </div>
             </div>
         </div>
-        <?php 
-            include('add_modal.php');
-        ?>
+        <?php include('add_modal.php'); ?>
     </body>
 </html>
+
+
