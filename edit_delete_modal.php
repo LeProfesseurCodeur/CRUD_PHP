e3c4c1792df910f58f5df7deef7e35870c4a4fa7
Dylan Luchmun <dylan.luchmun@epitech.eu>
2019-10-06 10:52:53 +0200

    initialization edit_delete file

---
+++ edit_delete_modal.php
@@ -0,0 +1,67 @@
+<!-- editer un user -->
+<div class="modal fade" id="edit_<?php echo $row['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            	 <center><h4 class="modal-title" id="myModalLabel">Editer un utilsateur</h4></center>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">
+			<div class="container-fluid">
+			<form method="POST" action="edit.php?id=<?php echo $row['id']; ?>">
+				<div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label" style="position:relative; top:7px;">Nom :</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="firstname" value="<?php echo $row['firstname']; ?>">
+					</div>
+				</div>
+				<div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label" style="position:relative; top:7px;">Prénom :</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="lastname" value="<?php echo $row['lastname']; ?>">
+					</div>
+				</div>
+				<div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label" style="position:relative; top:7px;">Adresse :</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="address" value="<?php echo $row['address']; ?>">
+					</div>
+				</div>
+            </div> 
+			</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-close"></span> Quitter</button>
+                <button type="submit" name="edit" class="btn btn-success"><span class="fa fa-check"></span> Update</a>
+			</form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- supprimer un user -->
+<div class="modal fade" id="delete_<?php echo $row['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            	<center><h4 class="modal-title" id="myModalLabel">Supprimer un utilisateur</h4></center>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">	
+            	<p class="text-center">Êtes-vous sûr de vouloir supprimer cet utilisateur ?</p>
+				<h2 class="text-center"><?php echo $row['firstname'].' '.$row['lastname']; ?></h2>
+			</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-close"></span> Non</button>
+                <a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger"><span class="fa fa-trash"></span> Oui</a>
+            </div>
+
+        </div>
+    </div>
+</div>
